4a83cd4d8ecd33930c4b3a8ee3a5166ab7ffda4f
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2019-08-02 15:48:39 -0400

    Add files via upload

---
+++ Assets/Scripts/IsCrushed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsCrushed : MonoBehaviour
+{
+    GameObject player;
+    
+    public GameObject character;
+    
+    void OnTriggerEnter2D(Collider2D other){
+
+       
+            
+            Destroy(character);
+        
+        
+
+    }
+}
